e97618ac24e9a4f4fcfc9a3aea533a8eee4f47a8
Carlos Romero <carlos.romero.breto@gmail.com>
2021-03-22 19:45:33 +0100

    add log event to subscribe/unsubscribe from map channel

--- app/channels/map_channel.rb
+++ app/channels/map_channel.rb
@@ -4,10 +4,12 @@ class MapChannel < ApplicationCable::Channel
     stream_from "map_channel"
 
     reject unless player_is_admin_or_owner
+
+    Rails.logger.info("Map Channel: #{current_player.nickname} subscribed.")
   end
 
   def unsubscribed
-
+    Rails.logger.info("Map Channel: #{current_player.nickname} unsubscribed.")
   end
 
 end
